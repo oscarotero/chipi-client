86e6f93b192bba8d4ad0953f750bd287f21993c0
Oscar Otero <oom@oscarotero.com>
2018-12-20 23:39:25 +0100

    updated to electron 4.0 stable

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const { app, ipcMain, globalShortcut } = require('electron');
-const settings = require('./main/settings');
 const { createWindow, hideWindow, toggleWindow } = require('./main/window');
 const { createMenu } = require('./main/menu');
 const { registerScheme, registerBufferProtocol } = require('./main/protocol');
@@ -12,21 +11,36 @@ if (process.env.NODE_ENV === 'development') {
 
 registerScheme();
 
-settings.watch('globalShortcut', (newValue, oldValue) => {
-    if (oldValue) {
-        globalShortcut.unregister(oldValue);
-    }
-
-    globalShortcut.register(newValue, () => toggleWindow(mainWindow));
-});
-
 app.on('ready', () => {
+    const settings = require('./main/settings');
+
     registerBufferProtocol();
+
     mainWindow = createWindow();
+
+    settings.watch('globalShortcut', (newValue, oldValue) => {
+        if (oldValue) {
+            globalShortcut.unregister(oldValue);
+        }
+
+        globalShortcut.register(newValue, () => toggleWindow(mainWindow));
+    });
 
     globalShortcut.register(settings.get('globalShortcut'), () => toggleWindow(mainWindow));
 
     createMenu(mainWindow);
+
+    app.on('browser-window-blur', (event, win) => {
+        if (win === mainWindow && settings.get('hideOnBlur')) {
+            hideWindow(mainWindow);
+        }
+    });
+
+    ipcMain.on('hide-window', () => {
+        if (settings.get('hideOnBlur')) {
+            hideWindow(mainWindow);
+        }
+    });
 });
 
 app.on('will-quit', () => globalShortcut.unregisterAll());
@@ -34,17 +48,5 @@ app.on('will-quit', () => globalShortcut.unregisterAll());
 app.on('activate', () => {
     if (mainWindow === null) {
         mainWindow = createWindow();
-    }
-});
-
-app.on('browser-window-blur', (event, win) => {
-    if (win === mainWindow && settings.get('hideOnBlur')) {
-        hideWindow(mainWindow);
-    }
-});
-
-ipcMain.on('hide-window', () => {
-    if (settings.get('hideOnBlur')) {
-        hideWindow(mainWindow);
     }
 });
